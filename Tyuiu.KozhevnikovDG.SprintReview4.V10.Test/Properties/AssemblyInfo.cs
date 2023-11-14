31c26d4c4aeb91dedf90123ae9ea01b19c2b22af
Tyuiu.KozhevnikovDG@outlook.com <f1zog2005@mail.ru>
2023-11-14 12:39:18 +0500

    Add project files.

---
+++ Tyuiu.KozhevnikovDG.SprintReview4.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KozhevnikovDG.SprintReview4.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KozhevnikovDG.SprintReview4.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f864c599-e824-41d8-b8ca-2e599458f22b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KozhevnikovDG.SprintReview4.V10/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KozhevnikovDG.SprintReview4.V10.Lib;
+namespace Tyuiu.KozhevnikovDG.SprintReview4.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;
+            int m = 4;
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнил: Кожевников Д.Г | ИИПб-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Кожевников Данил Георгиевич | ИИПб-23-3                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр 695847142536. Преобразуйте ее в       *");
+            Console.WriteLine("* матрицу 3 на 4 и подсчитайте количество не четных чисел.                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] mtrx = new int[n, m];
+            string value = "695847142536";
+
+            int index = 0;
+
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"{value[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            int res = ds.Calculate(n, m, value);
+
+            Console.WriteLine();
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+            Console.WriteLine("*************************************************************************************");
+
+            Console.WriteLine("Сумма нечетных элементов массива = " + res);
+            Console.ReadKey();
+        }
+    }
+}
+        
+    
+
